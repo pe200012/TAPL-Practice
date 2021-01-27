464bd80daa4d3f0da801ce1e77419a0716fa0060
pe200012 <1326263755@qq.com>
2021-01-27 11:37:27 +0800

    basic definition

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,44 @@
+{-# LANGUAGE DeriveAnyClass #-}
 module Main where
 
-import Lib
+import Prelude hiding (succ, pred)
+
+newtype Simple = Simple [Term]
+
+data Term = Boolean Bool
+          | NAT Nat
+          | IF Term Term Term
+          deriving Show
+
+data Nat = Zero
+         | Succ Nat
+         deriving (Show, Eq)
+
+{-|
+
+>>> isZero Zero
+Boolean True
+
+>>> isZero (Succ Zero)
+Boolean False
+
+-}
+isZero :: Nat -> Term
+isZero Zero = Boolean True
+isZero _    = Boolean False
+
+{-|
+
+>>> isZero (pred (succ Zero))
+Boolean True
+
+-}
+succ :: Nat -> Nat
+succ = Succ
+
+pred :: Nat -> Nat
+pred Zero     = error "Zero"
+pred (Succ n) = n
 
 main :: IO ()
-main = someFunc
+main = pure ()
