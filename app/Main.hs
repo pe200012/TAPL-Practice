eab5e556abec575d2974a1af0bb61f0aa9b55360
pe200012 <1326263755@qq.com>
2021-01-27 14:30:44 +0800

    induction on terms

--- app/Main.hs
+++ app/Main.hs
@@ -2,43 +2,72 @@
 module Main where
 
 import Prelude hiding (succ, pred)
+import Data.Set
 
 newtype Simple = Simple [Term]
 
-data Term = Boolean Bool
-          | NAT Nat
+data Term = TTrue
+          | TFalse
+          | Zero
+          | Succ Term
+          | Pred Term
+          | IsZero Term
           | IF Term Term Term
-          deriving Show
-
-data Nat = Zero
-         | Succ Nat
-         deriving (Show, Eq)
+          deriving (Show, Eq, Ord)
 
 {-|
 
 >>> isZero Zero
-Boolean True
+TTrue
 
 >>> isZero (Succ Zero)
-Boolean False
+TFalse
 
 -}
-isZero :: Nat -> Term
-isZero Zero = Boolean True
-isZero _    = Boolean False
+isZero :: Term -> Term
+isZero Zero = TTrue
+isZero _    = TFalse
 
 {-|
 
 >>> isZero (pred (succ Zero))
-Boolean True
+TTrue
 
 -}
-succ :: Nat -> Nat
+succ :: Term -> Term
 succ = Succ
 
-pred :: Nat -> Nat
+pred :: Term -> Term
 pred Zero     = error "Zero"
 pred (Succ n) = n
+
+consts :: Term -> Set Term
+consts Zero       = singleton Zero
+consts (Succ n)   = consts n
+consts (Pred n)   = consts n
+consts TTrue      = singleton TTrue
+consts TFalse     = singleton TFalse
+consts (IsZero n) = consts n
+consts (IF a b c) = consts a `union` consts b `union` consts c
+
+tSize :: Term -> Int
+tSize TTrue  = 1
+tSize TFalse = 1
+tSize Zero   = 1
+tSize (Succ n) = tSize n + 1
+tSize (Pred n) = tSize n + 1
+tSize (IsZero n) = tSize n + 1
+tSize (IF a b c) = sum (tSize <$> [a,b,c]) + 1
+
+depth :: Term -> Int
+depth TTrue  = 1
+depth TFalse = 1
+depth Zero   = 1
+depth (Succ n) = depth n + 1
+depth (Pred n) = depth n + 1
+depth (IsZero n) = depth n + 1
+depth (IF a b c) = maximum (depth <$> [a,b,c]) + 1
+
 
 main :: IO ()
 main = pure ()
