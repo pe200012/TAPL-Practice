41807c1cce93a0298848407e7bab7d9e151c97fd
pe200012 <1326263755@qq.com>
2021-02-03 18:20:31 +0800

    boolean functions

--- src/UntypedLambdaCalculus.hs
+++ src/UntypedLambdaCalculus.hs
@@ -1,5 +1,12 @@
 module UntypedLambdaCalculus where
 
+import           Prelude                 hiding ( and
+                                                , not
+                                                , or
+                                                , fst
+                                                , snd
+                                                )
+
 type Name = String
 
 data Term = Variable Name
@@ -7,8 +14,8 @@ data Term = Variable Name
           | Application (Term, Term)
 
 instance Show Term where
-    show (Variable n) = "Variable " ++ n
-    show (Abstraction _) = "Abstraction <opaque>"
+    show (Variable    n     ) = "Variable " ++ n
+    show (Abstraction _     ) = "Abstraction <opaque>"
     show (Application (a, b)) = "Application (" ++ show a ++ ", " ++ show b ++ ")"
 
 var :: Name -> Term
@@ -29,11 +36,50 @@ fls :: Term
 fls = abstract (\t -> abstract (\f -> f))
 
 {-
->>> debug (apply (apply (apply (test,tru),var "a"),var "b"))
-Variable a
+>>> f b = apply (apply (apply (test,b),var "true"),var "false")
 
->>> debug (apply (apply (apply (test,fls),var "a"),var "b"))
-Variable b
+>>> debug (f tru)
+Variable true
+
+>>> debug (f fls)
+Variable false
+
+>>> and' a b = apply (apply (and, a), b)
+
+>>> debug (f (and' tru tru))
+Variable true
+
+>>> debug (f (and' fls tru))
+Variable false
+
+>>> debug (f (and' tru fls))
+Variable false
+
+>>> debug (f (and' fls fls))
+Variable false
+
+>>> or' a b = apply (apply (or, a), b)
+
+>>> debug (f (or' tru tru))
+Variable true
+
+>>> debug (f (or' tru fls))
+Variable true
+
+>>> debug (f (or' fls tru))
+Variable true
+
+>>> debug (f (or' fls fls))
+Variable false
+
+>>> not' a = apply (not, a)
+
+>>> debug (f (not' tru))
+Variable false
+
+>>> debug (f (not' fls))
+Variable true
+
 -}
 
 -- | If-statement
@@ -42,9 +88,18 @@ test = abstract (\l -> abstract (\m -> abstract (\n -> apply (apply (l, m), n)))
 
 -- | simple evaluation, without involving variable substitution
 debug :: Term -> Term
-debug (Variable n) = Variable n
-debug (Abstraction f) = Abstraction f
+debug (Variable    n     ) = Variable n
+debug (Abstraction f     ) = Abstraction f
 debug (Application (a, b)) = case debug a of
-                                Abstraction f -> debug (f b')
-                                _             -> Application (a, b')
+    Abstraction f -> debug (f b')
+    _             -> Application (a, b')
     where b' = debug b
+
+and :: Term
+and = abstract (\b -> abstract (\c -> apply (apply (b, c), fls)))
+
+or :: Term
+or = abstract (\b -> abstract (\c -> apply (apply (b, tru), apply (apply (c, tru), fls))))
+
+not :: Term
+not = abstract (\b -> apply (apply (b, fls), tru))
