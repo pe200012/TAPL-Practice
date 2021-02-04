c11c02bef9bac8575a8726ee892ca3b2886b1681
pe200012 <1326263755@qq.com>
2021-02-04 20:38:34 +0800

    add, multiply, subtract on numerals

--- src/UntypedLambdaCalculus.hs
+++ src/UntypedLambdaCalculus.hs
@@ -1,12 +1,10 @@
+{-# LANGUAGE NoImplicitPrelude #-}
 module UntypedLambdaCalculus where
 
-import           Prelude                 hiding ( and
-                                                , not
-                                                , or
-                                                , fst
-                                                , snd
-                                                )
 
+
+
+import Prelude (String, Show(show), (++), const, undefined)
 type Name = String
 
 data Term = Variable Name
@@ -35,8 +33,11 @@ tru = abstract (\t -> abstract (\f -> t))
 fls :: Term
 fls = abstract (\t -> abstract (\f -> f))
 
+-- $setup
+-- >>> trueorfalse b = apply (apply (apply (test,b),var "true"),var "false")
+
 {-
->>> f b = apply (apply (apply (test,b),var "true"),var "false")
+>>> f = trueorfalse
 
 >>> debug (f tru)
 Variable true
@@ -126,3 +127,88 @@ fst = abstract (\p -> apply (p, tru))
 
 snd :: Term
 snd = abstract (\p -> apply (p, fls))
+
+{-
+
+>>> debug (trueorfalse (apply (iszro, c_0)))
+Variable true
+
+>>> debug (trueorfalse (apply (iszro, c_1)))
+Variable false
+
+>>> debug (trueorfalse (apply (iszro, apply (apply (times, c_0), c_1))))
+Variable true
+
+>>> debug (trueorfalse (apply (iszro, apply (prd, c_0))))
+Variable true
+
+>>> debug (trueorfalse (apply (iszro, apply (prd, c_1))))
+Variable true
+
+>>> debug (trueorfalse (apply (iszro, apply (prd, c_2))))
+Variable false
+
+>>> debug (trueorfalse (apply (iszro, apply (apply (subtract, c_1), c_1))))
+Variable true
+
+>>> debug (trueorfalse (apply (iszro, apply (apply (subtract, c_0), c_1))))
+Variable true
+
+>>> debug (trueorfalse (apply (iszro, apply (apply (subtract, c_2), c_1))))
+Variable false
+
+>>> debug (trueorfalse (apply (apply (equal, c_0), c_0)))
+Variable true
+
+>>> debug (trueorfalse (apply (apply (equal, c_1), c_0)))
+Variable false
+
+>>> debug (trueorfalse (apply (apply (equal, c_0), c_1)))
+Variable false
+
+>>> debug (trueorfalse (apply (apply (equal, c_2), apply (apply (times, c_1), c_2))))
+Variable true
+
+-}
+
+c_0 :: Term
+c_0 = abstract (\s -> abstract (\z -> z))
+
+c_1 :: Term
+c_1 = abstract (\s -> abstract (\z -> apply (s, z)))
+
+c_2 :: Term
+c_2 = abstract (\s -> abstract (\z -> apply (s, apply (s, z))))
+
+scc :: Term
+scc = abstract (\n -> abstract (\s -> abstract (\z -> apply (s, apply (apply (n, s), z)))))
+
+plus :: Term
+plus = abstract (\m -> abstract (\n -> abstract (\s -> abstract (\z -> apply (apply (m, s), apply (apply (n, s), z))))))
+
+times :: Term
+times = abstract (\m -> abstract (\n -> abstract (\s -> apply (m, apply (n, s)))))
+
+power :: Term
+power = abstract (\m -> abstract (\n -> apply (n, m)))
+
+iszro :: Term
+iszro = abstract (\m -> apply (apply (m, abstract (const fls)), tru))
+
+zz :: Term
+zz = apply (apply (pair, c_0), c_0)
+
+ss :: Term
+ss = abstract (\p -> apply (apply (pair, apply (snd, p)), apply (apply (plus, c_1), apply (snd, p))))
+
+prd :: Term
+prd = abstract (\m -> apply (fst, apply (apply (m, ss), zz)))
+
+subtract :: Term
+subtract = abstract (\m -> abstract (\n -> apply (apply (n, prd), m)))
+
+equal :: Term
+equal = abstract (\m -> abstract (\n -> apply (apply (and, r m n), r n m)))
+    where r m n = apply (iszro, apply (apply (subtract, m), n))
+
+
