777d1ca615ac8129052daca15c663f90be2bbc9e
pe200012 <1326263755@qq.com>
2021-07-19 22:38:58 +0800

    Subtyping: subtype inference

--- src/Subtyping.hs
+++ src/Subtyping.hs
@@ -1,22 +1,10 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ViewPatterns #-}
 module Subtyping where
 
+import           Control.Monad                  ( (<=<) )
 import           Data.Text.Format               ( format )
-import           Data.Text.Lazy                 ( Text
-                                                , unpack
-                                                )
-import           Data.Void                      ( Void )
-import           Text.Megaparsec                ( (<|>)
-                                                , MonadParsec(try)
-                                                , Parsec
-                                                , choice
-                                                , many
-                                                , optional
-                                                )
-import           Text.Megaparsec.Char           ( char
-                                                , digitChar
-                                                , space
-                                                )
+import           Data.Text.Lazy                 ( unpack )
 
 newtype Nat = Nat { unNat :: Int } deriving (Show, Eq)
 
@@ -25,7 +13,7 @@ instance Num Nat where
     Nat a * Nat b = Nat (a * b)
     abs    = id
     signum = const 1
-    fromInteger x | x < 0     = error "Nat should be positive"
+    fromInteger x | x < 0     = error "Nat should be non-negative"
                   | otherwise = Nat (fromIntegral x)
     negate = error "undefined negate on Nat"
 
@@ -33,11 +21,21 @@ data Term = Index Int
           | Abstraction Type Term
           | Application Term Term
           | NaturalNumber Nat
-    deriving Show
+    deriving (Show, Eq)
 
 data Type = Type :-> Type
           | NatNumber
+          | Top
+          | Bottom
     deriving (Show, Eq)
+
+data Subtype = Type :<: Type
+
+isSubtype :: Type -> Type -> Bool
+isSubtype _      Top = True
+isSubtype Bottom _   = True
+isSubtype (a :-> b) (c :-> d) | isSubtype c a && isSubtype b d = True
+isSubtype a b        = a == b
 
 prettyTerm :: Term -> String
 prettyTerm (Index i          ) = show i
@@ -61,16 +59,17 @@ subst t@(NaturalNumber _   ) p      = t
 typeshift :: Int -> Int -> Type -> Type
 typeshift distance cutoff (a :-> b) = typeshift distance cutoff a :-> typeshift distance cutoff b
 typeshift distance cutoff NatNumber = NatNumber
+typeshift distance cutoff Top       = Top
+typeshift distance cutoff Bottom    = Bottom
 
 typesubst :: Type -> (Int, Type) -> Type
 typesubst (a :-> b) p = typesubst a p :-> typesubst b p
 typesubst NatNumber p = NatNumber
+typesubst Top       p = Top
+typesubst Bottom    p = Bottom
 
 typesubst' :: Type -> Type -> Type
 typesubst' t u = typeshift (-1) 0 (typesubst (typeshift 1 0 t) (0, u))
-
--- >>> typing (Fold (Mu (Mu (TypeVariable 1))) (Unfold (Mu (Mu (TypeVariable 1))) (Fold (Mu (Mu (TypeVariable 1))) (Fix (Abstraction (Mu (TypeVariable 0)) (Index 0))))))
--- Right (Mu (Mu (TypeVariable 0)))
 
 typing :: Term -> Either String Type
 typing = go [] []
@@ -82,16 +81,33 @@ typing = go [] []
         a' <- go vars typecxt a
         b' <- go vars typecxt b
         case a' of
-            t :-> u | t == b'   -> Right u
-                    | otherwise -> Left (unpack $ format "typing: type mismatch. Expected {}, got {}" [show t, show b'])
+            Bottom -> Right Bottom
+            t :-> u | isSubtype b' t -> Right u
+                    | otherwise      -> Left (unpack $ format "typing: type mismatch. Expected {}, got {}" [show t, show b'])
             _ -> Left (unpack $ format "typing: type mismatch. Expected arrow type, got {}" [show a'])
     go vars typecxt (NaturalNumber _) = return NatNumber
 
 eval :: Term -> Term
-eval (Index x          ) = Index x
+eval (Index x        ) = Index x
 eval (Abstraction typ t) = Abstraction typ (eval t)
-eval (Application a   b) = eval (shift (-1) 0 (subst a (0, shift 1 0 b)))
-eval (NaturalNumber i  ) = NaturalNumber i
+eval (Application (eval -> Abstraction _ a) b) = eval (shift (-1) 0 (subst a (0, shift 1 0 b)))
+eval (Application a b) = Application a b
+eval (NaturalNumber i) = NaturalNumber i
+
+-- >>> shift 1 0 (NaturalNumber 1)
+-- NaturalNumber (Nat {unNat = 1})
+
+-- >>> (subst (Index 0) (0, NaturalNumber 1))
+-- NaturalNumber (Nat {unNat = 1})
+
+-- >>> eval (subst (Index 0) (0, NaturalNumber 1))
+-- NaturalNumber (Nat {unNat = 1})
+
+-- >>> eval (Application (Abstraction NatNumber (Index 0)) (NaturalNumber 2))
+-- NaturalNumber (Nat {unNat = 2})
+
+-- prop> \x -> eval (Application (Abstraction NatNumber (Index 0)) (NaturalNumber (Nat x))) == NaturalNumber (Nat {unNat = x})
+-- +++ OK, passed 100 tests.
 
 eval' :: Term -> Term
 eval' = either error . const . eval <*> typing
